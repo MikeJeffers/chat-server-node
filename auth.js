b88e0ca698edb9bc89c6a0bfeb05856e4361dde7
mikejeffers <mike.s.jeffers@gmail.com>
2024-01-20 01:28:52 -0500

    flatten claims in the jwt

--- auth.js
+++ auth.js
@@ -11,9 +11,9 @@ const USER = require('./user');
 module.exports = async (token) => {
   if (!token) { throw new Error('No token provided'); }
   const decoded = jwt.verify(token, process.env.SECRET_JWT);
-  const fetchedToken = await Redis.get(`jwt:${decoded.user.id}`);
+  const fetchedToken = await Redis.get(`jwt:${decoded.id}`);
   if (fetchedToken != token) {
     throw new Error('bad token');
   }
-  return await USER.getById(decoded.user.id);
+  return await USER.getById(decoded.id);
 };
